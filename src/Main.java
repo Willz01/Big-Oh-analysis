dd518dfd4cd995007a155cb02ad2b3dd42f31ebd
Willz.01 <ekanemwills1@gmail.com>
2020-11-18 16:59:49 +0100

    Binary search implemented, quicksort implemented as well.Crushed bugs!

--- src/Main.java
+++ src/Main.java
@@ -76,10 +76,27 @@ public class Main {
 							System.out.println("QUICK SORT");
 							// we call this everytime so that the algorithm can have a new unsorted array everytime.
 							arr = inputSizeArr(takeSize());
-							Long sTime = System.nanoTime();
-							quickSort(arr);
-							Long runtime = (System.nanoTime() - sTime);
-							displayRunTime(runtime);
+							ArrayList<Integer> unsorted = arr;
+							// Median of three pivot
+							Long sTimeMedian = System.nanoTime();
+							quickSortMedianOfThree(arr);
+							Long runtimeMedian = (System.nanoTime() - sTimeMedian);
+							System.out.println("MEDIAN OF THREE PIVOT");
+							displayRunTime(runtimeMedian);
+							// Random pivot
+							arr = unsorted;
+							Long sTimeRandom = System.nanoTime();
+							quickSortRandomIndexPivot(arr);
+							Long runtimeRandom = (System.nanoTime() - sTimeRandom);
+							System.out.println("RANDOM PIVOT");
+							displayRunTime(runtimeRandom);
+							// pivot = array[0]
+							arr = unsorted;
+							Long sTimeIndexZero = System.nanoTime();
+							quickSortIndexZeroPivot(arr);
+							Long runTimeIndexZero = (System.nanoTime() - sTimeIndexZero);
+							System.out.println("PIVOT = ARRAY[0]");
+							displayRunTime(runTimeIndexZero);
 							break;
 						case 2:
 							System.out.println("MERGE SORT");
@@ -104,13 +121,17 @@ public class Main {
 						case 4:
 							System.out.println("BINARY SEARCH");
 							arr = inputSizeArr(takeSize());
-							Long sTimeBinarySearch = System.nanoTime();
 							System.out.print("Enter number to search for: ");
 							numberToFind = input.nextInt();
 							input.nextLine();
-							binarySearch(arr, numberToFind);
+							// sort the array for binary search
+							mergeSort(arr, 0, arr.size() - 1);
+							Long sTimeBinarySearch = System.nanoTime();
+							boolean result = binarySearch(arr, 0, arr.size() - 1, numberToFind);
 							Long runtimeBinarySearch = (System.nanoTime() - sTimeBinarySearch);
 							displayRunTime(runtimeBinarySearch);
+							if(result == true) System.out.println("Number exists!");
+							else System.out.println("Number does not exists!");
 							break;
 						case 5:
 							System.out.println("Thanks for using!");
@@ -123,7 +144,7 @@ public class Main {
 			} else if(choice1 == 2) {
 				// quick sort
 				AverageRunTime averageRunTime = new AverageRunTime();
-				System.out.println("not yet implemented");
+				averageRunTime.runAllQuickSort();
 
 			} else if(choice1 == 3) {
 				// insertion sort
@@ -136,7 +157,7 @@ public class Main {
 			} else if(choice1 == 5) {
 				// binary search
 				AverageRunTime averageRunTime = new AverageRunTime();
-				System.out.println("not yet implemented");
+				averageRunTime.runBinarySearch();
 
 			} else if(choice1 == 6) {
 				doneExec = true;
@@ -213,9 +234,122 @@ public class Main {
 	/**
 	 * @param arr - ArrayList to implement algorithm on
 	 */
-	public static void quickSort(ArrayList<Integer> arr) {
+	public static void quickSort(ArrayList<Integer> arr, int sortType) {
 //		System.out.println("QUICK SORT");
 		/** code **/
+		if(sortType == 1) {
+			quickSortMedianOfThree(arr);
+		} else if(sortType == 2) {
+			quickSortRandomIndexPivot(arr);
+		} else if(sortType == 3) {
+			quickSortIndexZeroPivot(arr);
+		} else {
+			System.out.println("Invalid input!");
+		}
+	}
+
+	public static void quickSortIndexZeroPivot(ArrayList<Integer> arr) {
+		int size = arr.size();
+		// base case
+		if(size <= 1) return;
+
+		ArrayList<Integer> smaller = new ArrayList<>();
+		ArrayList<Integer> same = new ArrayList<>();
+		ArrayList<Integer> larger = new ArrayList<>();
+
+		int pivot = arr.get(0);
+
+		for(int i = 0; i < arr.size(); i++) {
+			if(pivot > arr.get(i)) {
+				smaller.add(arr.get(i));
+			} else if(pivot < arr.get(i)) {
+				larger.add(arr.get(i));
+			} else {
+				same.add(arr.get(i));
+			}
+		}
+		// recursive call
+		quickSortIndexZeroPivot(smaller);
+		quickSortIndexZeroPivot(larger);
+
+		arr.clear();
+		arr.addAll(smaller);
+		arr.addAll(same);
+		arr.addAll(larger);
+	}
+
+	public static void quickSortRandomIndexPivot(ArrayList<Integer> arr) {
+		int size = arr.size();
+		// base case
+		if(size <= 1) return;
+
+		ArrayList<Integer> smaller = new ArrayList<>();
+		ArrayList<Integer> same = new ArrayList<>();
+		ArrayList<Integer> larger = new ArrayList<>();
+
+		// get random index between o(inclusize) and size of arraylist(exclusize)
+		Random random = new Random();
+		int num = random.nextInt(arr.size());
+		int pivot = arr.get(num);
+
+		for(int i = 0; i < arr.size(); i++) {
+			if(pivot > arr.get(i)) {
+				smaller.add(arr.get(i));
+			} else if(pivot < arr.get(i)) {
+				larger.add(arr.get(i));
+			} else {
+				same.add(arr.get(i));
+			}
+		}
+		// recursive call
+		quickSortRandomIndexPivot(smaller);
+		quickSortRandomIndexPivot(larger);
+
+		arr.clear();
+		arr.addAll(smaller);
+		arr.addAll(same);
+		arr.addAll(larger);
+	}
+
+	public static void quickSortMedianOfThree(ArrayList<Integer> arr) {
+		int size = arr.size();
+		// base case
+		if(size <= 1) return;
+
+		ArrayList<Integer> smaller = new ArrayList<>();
+		ArrayList<Integer> same = new ArrayList<>();
+		ArrayList<Integer> larger = new ArrayList<>();
+
+		// Getting the median of 3
+		// the nth element
+		int n = arr.get(arr.size() - 1);
+		// the first element
+		int zero = arr.get(0);
+		// the middle of the array
+		int mid = arr.get(arr.size() / 2);
+		// put all in an array and use the Arrays sort method and retrieve the middle of the array.
+		int[] ar = {n, zero, mid};
+		Arrays.sort(ar);
+		int pivot = ar[1];
+
+		for(int i = 0; i < arr.size(); i++) {
+			if(pivot > arr.get(i)) {
+				smaller.add(arr.get(i));
+			} else if(pivot < arr.get(i)) {
+				larger.add(arr.get(i));
+			} else {
+				same.add(arr.get(i));
+			}
+		}
+
+		// recursive call
+		quickSortMedianOfThree(smaller);
+		quickSortMedianOfThree(larger);
+
+		arr.clear();
+		arr.addAll(smaller);
+		arr.addAll(same);
+		arr.addAll(larger);
 	}
 
 	/**
@@ -238,10 +372,17 @@ public class Main {
 	 * @param number - number to search for
 	 * @return - if number exists in sort ArrayList return true, otherwise false.
 	 */
-	public static boolean binarySearch(ArrayList<Integer> arr, int number) {
-//		System.out.println("BINARY SEARCH");
+	public static boolean binarySearch(ArrayList<Integer> arr, int left, int right, int numberToFind) {
 		/** code **/
-		return true;
+		if(right >= left) {
+			int mid = left + (right - left) / 2;
+			if(arr.get(mid) == numberToFind)
+				return true;
+			if(arr.get(mid) > numberToFind)
+				return binarySearch(arr, left, mid - 1, numberToFind);
+			return binarySearch(arr, mid + 1, right, numberToFind);
+		}
+		return false;
 	}
 
 	/**
@@ -294,7 +435,7 @@ public class Main {
 			if(leftIndex < leftArray.length && rightIndex < rightArray.length) {
 				/**
 				 * Getting the smallest:
-				 * if the left index of left array is smaller, add that else work it for right
+				 * if the left index of left array is smaller add that to the array otherwise add the right
 				 */
 				if(leftArray[leftIndex] < rightArray[rightIndex]) {
 					arr.set(i, leftArray[leftIndex]);
@@ -304,11 +445,11 @@ public class Main {
 					rightIndex++;
 				}
 			} else if(leftIndex < leftArray.length) {
-				// if no numbers in right array, copy the rest of left array
+				// if no numbers in right array, copy the rest of left array (because the left array is already sorted)
 				arr.set(i, leftArray[leftIndex]);
 				leftIndex++;
 			} else if(rightIndex < rightArray.length) {
-				// if no numbers in left array, copy the rest of right
+				// if no numbers in left array, copy the rest of right (because the right array is already sorted)
 				arr.set(i, rightArray[rightIndex]);
 				rightIndex++;
 			}
@@ -340,6 +481,8 @@ public class Main {
 		// test
 		Long[] testInsertionSort = new Long[11];
 		Long[] testMergeSort = new Long[11];
+		Scanner input = new Scanner(System.in);
+		boolean exist = false;
 
 		AverageRunTime() throws IOException {
 		}
@@ -347,12 +490,55 @@ public class Main {
 		/**
 		 * @return average run time of quicksort algorithm ran 5 times.
 		 */
-		// TODO - parameter of number of runs from user input
-		Long avgRunTimeQuicksort() {
+		Long avgRunTimeQuicksortMedianOfThree() {
 			Long totalRunTime = 0L;
 			for(int i = 0; i < 11; i++) {
 				Long startTime = System.nanoTime();
-				quickSort(arrayList);
+				quickSortMedianOfThree(arrayList);
+				Long runTime = (System.nanoTime() - startTime);
+				arrayList = unsortedArrayList;
+				/**
+				 * Calculating avg from the second run time because the first run time has complilation cost.
+				 * So from the test array logged on the console, the first number will always be way higher than the rest
+				 * Adding that high number to the Array of runs times just for obs.
+				 */
+				if(i >= 1) {
+					totalRunTime += runTime;
+				}
+			}
+			return totalRunTime / 10;
+		}
+
+		/**
+		 * @return average run time of quicksort algorithm ran 5 times.
+		 */
+		Long avgRunTimeQuicksortRandomPivot() {
+			Long totalRunTime = 0L;
+			for(int i = 0; i < 11; i++) {
+				Long startTime = System.nanoTime();
+				quickSortRandomIndexPivot(arrayList);
+				Long runTime = (System.nanoTime() - startTime);
+				arrayList = unsortedArrayList;
+				/**
+				 * Calculating avg from the second run time because the first run time has complilation cost.
+				 * So from the test array logged on the console, the first number will always be way higher than the rest
+				 * Adding that high number to the Array of runs times just for obs.
+				 */
+				if(i >= 1) {
+					totalRunTime += runTime;
+				}
+			}
+			return totalRunTime / 10;
+		}
+
+		/**
+		 * @return average run time of quicksort algorithm ran 5 times.
+		 */
+		Long avgRunTimeQuicksortIndexZeroPivot() {
+			Long totalRunTime = 0L;
+			for(int i = 0; i < 11; i++) {
+				Long startTime = System.nanoTime();
+				quickSortIndexZeroPivot(arrayList);
 				Long runTime = (System.nanoTime() - startTime);
 				arrayList = unsortedArrayList;
 				/**
@@ -420,9 +606,11 @@ public class Main {
 		 */
 		Long avgBinarySearch() throws IOException {
 			Long totalRunTime = 0L;
+
 			for(int i = 0; i < 11; i++) {
+				mergeSort(arrayList, 0, arrayList.size() - 1);
 				Long startTime = System.nanoTime();
-				binarySearch(arrayList, numberToFind);
+				exist = binarySearch(arrayList, 0, arrayList.size() - 1, numberToFind);
 				Long runTime = (System.nanoTime() - startTime);
 				arrayList = unsortedArrayList;
 				/**
@@ -447,7 +635,7 @@ public class Main {
 		void runAll() throws IOException {
 			unsortedArrayList = inputSizeArr(takeSize());
 			arrayList = unsortedArrayList;
-			System.out.println("Average run time - quicksort: " + avgRunTimeQuicksort() + " nanoseconds.");
+//			System.out.println("Average run time - quicksort: " + avgRunTimeQuicksort() + " nanoseconds.");
 			// test
 			System.out.println(arrayList.size());
 			System.out.println(unsortedArrayList.size());
@@ -457,16 +645,50 @@ public class Main {
 		void runInsertion() throws IOException {
 			unsortedArrayList = inputSizeArr(takeSize());
 			arrayList = unsortedArrayList;
-			System.out.println("Average run time - Insertion sort: " + avgRunTimeInsertionSort() + " nanoseconds.");
+			System.out.println("Average run time - Insertion sort: ");
+			displayRunTime(avgRunTimeInsertionSort());
 			System.out.println(Arrays.toString(testInsertionSort));
 		}
 
 		void runMergeSort() throws IOException {
 			unsortedArrayList = inputSizeArr(takeSize());
 			arrayList = unsortedArrayList;
-			System.out.println("Average run time - Merge sort: " + avgRunTimeMergeSort() + " nanoseconds.");
+			System.out.println("Average run time - Merge sort: ");
+			displayRunTime(avgRunTimeMergeSort());
 			System.out.println(Arrays.toString(testMergeSort));
 		}
+
+		void runBinarySearch() throws IOException {
+			unsortedArrayList = inputSizeArr(takeSize());
+			arrayList = unsortedArrayList;
+
+			System.out.print("Enter number to search for: ");
+			numberToFind = input.nextInt();
+			input.nextLine();
+
+			System.out.println("Average run time - Binary search: ");
+			displayRunTime(avgBinarySearch());
+			if(exist == true) System.out.println("Number exists!");
+			else System.out.println("Number does not exists!");
+		}
+
+		void runAllQuickSort() throws IOException {
+			unsortedArrayList = inputSizeArr(takeSize());
+			arrayList = unsortedArrayList;
+			System.out.println("Average run time - Quick sort median of three pivot: ");
+			displayRunTime(avgRunTimeQuicksortMedianOfThree());
+
+			arrayList = unsortedArrayList;
+			System.out.println("Average run time - Quick sort random pivot: ");
+			displayRunTime(avgRunTimeQuicksortRandomPivot());
+
+			arrayList = unsortedArrayList;
+			System.out.println("Average run time - Quick sort index zero pivot: ");
+			displayRunTime(avgRunTimeQuicksortIndexZeroPivot());
+
+
+		}
+
 	}
 }
 
